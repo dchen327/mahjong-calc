35fd70fbf8e1d18a2925a772da929370cff6c307
David Chen <davidc3287@gmail.com>
2025-07-20 17:28:48 -0400

    clean up manifest

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -46,16 +46,8 @@ const manifest = {
   },
   content_scripts: [
     {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content/all.iife.js'],
-    },
-    {
       matches: ['https://example.com/*'],
       js: ['content/example.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      css: ['content.css'],
     },
     {
       matches: ['https://mahjongsoft.com/*'],
